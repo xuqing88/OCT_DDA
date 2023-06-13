0bb6a9242c7078959508a1eaa6d2c156c4c65b23
Xu Qing <42113623+xuqing88@users.noreply.github.com>
2023-06-13 13:00:40 +0800

    Create mmd.py

---
+++ mmd.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import torch
+from layers import SinkhornDistance
+
+
+def mmd_linear(f_of_X, f_of_Y):
+    delta = f_of_X - f_of_Y
+    loss = torch.mean(torch.mm(delta, torch.transpose(delta, 0, 1)))
+    return loss
+
+def CORAL_loss(source, target):
+    d = source.data.shape[1]
+
+    # source covariance
+    xm = torch.mean(source, 1, keepdim=True) - source
+    xc = torch.matmul(torch.transpose(xm, 0, 1), xm)
+
+    # target covariance
+    xmt = torch.mean(target, 1, keepdim=True) - target
+    xct = torch.matmul(torch.transpose(xmt, 0, 1), xmt)
+    # frobenius norm between source and target
+    loss = (xc - xct).pow(2).sum().sqrt()
+    loss = loss/(4*d*d)
+    return loss
+    
+def poly_mmd2(f_of_X, f_of_Y, d=2, alpha=1.0, c=2.0):
+    K_XX = (alpha * (f_of_X[:-1] * f_of_X[1:]).sum(1) + c)
+    K_XX_mean = torch.mean(K_XX.pow(d))
+
+    K_YY = (alpha * (f_of_Y[:-1] * f_of_Y[1:]).sum(1) + c)
+    K_YY_mean = torch.mean(K_YY.pow(d))
+
+    K_XY = (alpha * (f_of_X[:-1] * f_of_Y[1:]).sum(1) + c)
+    K_XY_mean = torch.mean(K_XY.pow(d))
+
+    K_YX = (alpha * (f_of_Y[:-1] * f_of_X[1:]).sum(1) + c)
+    K_YX_mean = torch.mean(K_YX.pow(d))
+
+    return K_XX_mean + K_YY_mean - K_XY_mean - K_YX_mean
+    
+def sinhorn_loss(f_of_X,f_of_Y):
+    sinkhorn = SinkhornDistance(eps=0.1, max_iter=100, reduction=None)
+    dist, P, C = sinkhorn(f_of_X, f_of_Y)
+    return dist
+    #print("Sinkhorn distance: {:.3f}".format(dist.item()))
